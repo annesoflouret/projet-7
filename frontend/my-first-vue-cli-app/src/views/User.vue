c0d057b173474d6e5abd73689670ece4b2da3f1c
annesoflouret <annesophieflouret@gmail.com>
2020-08-27 22:04:34 +0200

    create comment and user pages frontend

---
+++ frontend/my-first-vue-cli-app/src/views/User.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <b-row>
+      <b-col sm="8">
+        <h3>Mon profil</h3>
+      </b-col>
+      <b-col sm="4">
+        <div class="float-right">
+          <b-button v-on:click="deleteUser" variant="danger" class="ml-2">Supprimer</b-button>
+        </div>
+      </b-col>
+    </b-row>
+    <hr />
+    <b-alert
+      show
+      dismissible
+      fade
+      variant="success"
+      v-if="$route.params.message"
+    >{{ $route.params.message }}</b-alert>
+
+    <p>Username: {{ user.username }}</p>
+    <p>Email : {{ user.email }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "User",
+  components: {},
+  data() {
+    return {
+      user: {},
+    };
+  },
+  methods: {
+    async deleteUser() {
+      try {
+        let response = await axios.delete("user/me");
+        console.log(response);
+        localStorage.clear();
+
+        document.location.reload(true);
+        this.$router.replace({
+          name: "login",
+          params: { message: "Compte supprimé !" },
+        });
+      } catch (err) {
+        this.error = err.response.data.error;
+      }
+    },
+  },
+  mounted() {
+    axios.get("user/me").then((response) => {
+      this.user = response.data;
+    });
+  },
+};
+</script>
