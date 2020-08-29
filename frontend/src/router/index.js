a494d85e44551061c29bb1224ba396bd359523ae
annesoflouret <annesophieflouret@gmail.com>
2020-08-29 23:25:26 +0200

    change Home to Login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,19 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 import Posts from '../views/Posts.vue'
 import User from '../views/User.vue'
 import Signup from '../views/Signup.vue'
-import Comments from '../views/Comments.vue'
-
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/posts',
@@ -29,11 +27,6 @@ const routes = [
     path: '/signup',
     name: 'signup',
     component: Signup
-  },
-  {
-    path: '/comments',
-    name: 'comments',
-    component: Comments
   }
 ]
 
