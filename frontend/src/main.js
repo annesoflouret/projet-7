48324bc19a51e18a872874379111bc8a82430492
annesoflouret <annesophieflouret@gmail.com>
2020-08-29 23:24:27 +0200

    add store and moment

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,8 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from './store'
+import moment from 'moment'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
@@ -13,7 +15,11 @@ Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
+moment.locale('fr')
+Vue.prototype.moment = moment
+
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
