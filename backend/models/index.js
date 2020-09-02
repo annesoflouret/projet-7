07cb388d12a33cb5ddb762d482bf0152b7cdc0c3
annesoflouret <annesophieflouret@gmail.com>
2020-09-02 23:46:40 +0200

    remove socket

--- backend/models/index.js
+++ backend/models/index.js
@@ -7,18 +7,10 @@ const db = {};
 let sequelize;
 
 // Connexion à la base de donnée
-let dialectOptions;
-if (process.env.DB_SOCKET == '') {
-  dialectOptions = {};
-}else {
-  dialectOptions = { socketPath: process.env.DB_SOCKET};
-}
-
 sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: 'mysql',
-  dialectOptions: dialectOptions
+  dialect: 'mysql'
 });
 
 fs
